f4890e52388ed616c95323744d41e67a3d6c33d2
y.yakubovich <y.yakubovich@softteco.com>
2024-04-10 10:53:07 +0300

    modified launchSettings.json added Program.cs Class ServiceExtentions with Extention Methods:  - ConfigureCors  - ConfigureIISIntegration added CORS and IIS configuration to Program.cs added Production Environment Base Settings file

---
+++ UltimateAspDotNetCoreWebApi/1_1-1_7/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace _1_1_1_7.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+
+    public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+
+        });
+}
